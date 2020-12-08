1c8c7c50e09155225fffca9a13a7bb106ee8149f
winnie <asgh03226@gmail.com>
2020-12-08 15:17:41 +0800

    saved addlist

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,26 +1,45 @@
 // TO DO
+
+
 document.addEventListener("DOMContentLoaded",function(){
 
-//create a listbtn
-  const creatBtn = document.querySelector(".addBtn")
-
-  const inputTxt = null
-
-  creatBtn.addEventListener("click",addList)
-
-
-
-})
-
-
-function addList(){
-
-  // console.log("click")
-
+  //create a listbtn
+    const creatBtn = document.querySelector(".addBtn")
+  
+    creatBtn.addEventListener("click",addList)
 
   
+  })
+  
+  
+  function addList(){
+  
+    // console.log("click")
 
+  
+    let addElement = document.getElementById('input').value
+    document.getElementById('input').value=""
+  
 
+    if(addElement!=""){
 
+      let listBox = document.querySelector('ul')
+    
+      let li = document.createElement("li")
 
-}
+      li.innerHTML = addElement + '<span class="close">x</span>'
+
+      
+    
+      listBox.appendChild(li)
+     
+
+      return (li)
+
+    }else {
+      alert( "請輸入文字！");
+    }
+
+    
+  
+  }
