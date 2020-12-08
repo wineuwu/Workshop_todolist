a2afb71534f59391d97fb7d7ab2f200afd45e3ff
winnie <asgh03226@gmail.com>
2020-12-08 11:29:16 +0800

    addList init saved

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,26 @@
 // TO DO
+document.addEventListener("DOMContentLoaded",function(){
+
+//create a listbtn
+  const creatBtn = document.querySelector(".addBtn")
+
+  const inputTxt = null
+
+  creatBtn.addEventListener("click",addList)
+
+
+
+})
+
+
+function addList(){
+
+  // console.log("click")
+
+
+  
+
+
+
+
+}
